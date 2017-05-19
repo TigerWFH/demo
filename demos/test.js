1b305fc06649645c7b4e4c362e3322667c1ecc3a
monkey-office <monkey@lwork.com>
2017-05-19 16:32:42 +0800

    curry测试

--- demos/test.js
+++ demos/test.js
@@ -1,71 +1,55 @@
-/*
-	订阅/发布
-*/
+function add() {
+	let l = arguments.length;
+	let sum = 0;
+	while (l > 0) {
+		sum += arguments[--l];
+	}
 
-class Product {
-	constructor() {
-		this.clientList = [];
-	}
-	registry(client) {
-		this.clientList.push(client);
-	}
-	notify(info) {
-		this.clientList.forEach((elem) => {
-			elem.receiveInfo(info);
-		});
+	// sum = Array.prototype.reduce.call(argements, function(pre,cur){
+	// 	return pre + cur;
+	// });
+
+	return sum;
+}
+
+function curry(fn) {
+	let argList = Array.prototype.slice.call(arguments, 1);
+	return function () {
+		let innerArgList = Array.prototype.slice.call(arguments);
+		let finalArgList = argList.concat(innerArgList);
+		return fn.apply(null, finalArgList);
+	};
+}
+
+//f()=>0
+// f(3)()=>3
+// f(3)(5)()=>8
+// f(3)(5)(9)()=>17
+function curry1(fn) {
+	let _args = arguments;
+	let argList = Array.prototype.slice.call(_args, 1);
+	return function () {
+		let innerArgList = Array.prototype.slice.call(arguments);
+		let finalArgList = argList.concat(innerArgList);
+		if (innerArgList.length > 0) {
+			argList = finalArgList;
+			return arguments.callee;
+		}
+
+		argList = Array.prototype.slice.call(_args, 1);
+		return fn.apply(null, finalArgList);
 	}
 }
 
-class Client {
-	constructor(name) {
-		this.name = name;
-	}
-	receiveInfo(info) {
-		console.log('info--->', info);
-	}
-}
-
-let pro = new Product();
-let c1 = new Client("monkey");
-pro.registry(c1);
-let c2 = new Client("cat");
-pro.registry(c2);
-pro.notify('二手东降价了');
-/*
-	生产者与消费者
-*/
-
-class Producer {
-	constructor() {
-		this.source = [];
-	}
-	myDispatch(type) {
-		this.source.forEach((elem) => {
-			if (elem.type === type) {
-				elem.handler();
-			}
-		});
-	}
-	myRegistry(type, handler) {
-		this.source.push({
-			type: type,
-			handler: handler
-		});
-	}
-}
-class Consumer {
-	constructor(name) {
-		this.name = name;
-	}
-	sayHello(producer) {
-		producer.myDispatch('say');
-	}
-}
-
-let p = new Producer();
-let c3 = new Consumer("elephant");
-p.myRegistry('say', () => {
-	console.log('say hello');
-});
-c3.sayHello(p);
-
+// 柯里化可以提前把操作共同的部分埋进去柯里化函数中，而不用暴露出来
+let curriedAdd = curry(add, 100);//100才体现了柯里化的作用,所有的求和都会算进该100参数
+let curriedAdd2 = curry1(add);
+// 1
+console.log(curriedAdd());//0
+console.log(curriedAdd(1, 2));//3
+console.log(curriedAdd(1, 7, 9));//17
+// 2
+console.log(curriedAdd2());//0
+console.log(curriedAdd2(1)());//1
+console.log(curriedAdd2(1)(2)());//3
+console.log(curriedAdd2(1)(2)(9)());//12
