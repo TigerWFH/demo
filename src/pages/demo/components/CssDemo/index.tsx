dc3bed69648fe960f729e9c5ef4d9382b56fb545
wangfanghua <wangfanghua@jk.cn>
2017-08-15 09:49:23 +0800

    使用text-overflow控制当行文本内容

--- src/pages/demo/components/CssDemo/index.tsx
+++ src/pages/demo/components/CssDemo/index.tsx
@@ -12,8 +12,11 @@ class CssDemo extends React.Component<IProps, IState>{
         return (
             <div>
                 <h1>对文本的控制</h1>
-                <div>
-                    <span>
+                <div className="text-first">
+                    <h3 style={{ textAlign: "center" }}>
+                        不限定宽高，自动折行
+                    </h3>
+                    <span className="first-span">
                         我是span，不限定宽高，会自动换行适应文本的长度；
                         我是span，不限定宽高，会自动换行适应文本的长度；
                         我是span，不限定宽高，会自动换行适应文本的长度；
@@ -25,12 +28,41 @@ class CssDemo extends React.Component<IProps, IState>{
                         我是span，不限定宽高，会自动换行适应文本的长度；
                         我是span，不限定宽高，会自动换行适应文本的长度；
                     </span>
-                    <div>
+                    <div className="first-div">
                         我是div，不限定宽高，会自动换行适应文本的长度；
                         我是div，不限定宽高，会自动换行适应文本的长度；
                         我是div，不限定宽高，会自动换行适应文本的长度；
                         我是div，不限定宽高，会自动换行适应文本的长度；
                     </div>
+                </div>
+                <div className="text-second">
+                    <h3 style={{ textAlign: "center" }}>
+                        限定宽高
+                    </h3>
+                    <span className="second-span">
+                        inline元素，width无效，自动折行。
+                        我是span，限定宽高，会自动换行适应文本的长度；
+                        我是span，限定宽高，会自动换行适应文本的长度；
+                        我是span，限定宽高，会自动换行适应文本的长度；
+                        我是span，限定宽高，会自动换行适应文本的长度；
+                     </span>
+                    <div className="second-div second-div-1">
+                        block元素，只限定宽度，自动撑满高度；
+                        限定宽高，文本会溢出block元素。
+                        overflow可以处理溢出内容。
+                    </div>
+                    <div className="second-div second-div-2">
+                        block元素，只限定宽度，自动撑满高度；
+                        限定宽高，文本会溢出block元素。
+                        white-space控制是否折行,
+                        overflow可以处理溢出内容。
+                    </div>
+                    <div className="second-div second-div-3">
+                        text-overflow必须配合white-space和overflow使用。
+                        white-space控制是否折行,
+                        overflow可以处理溢出内容。
+                    </div>
+
                 </div>
             </div>
         )
