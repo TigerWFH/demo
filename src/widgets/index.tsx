a973edc591f677184ff77b888946703ac5ca07e1
wangfanghua <wangfanghua@jk.cn>
2017-08-21 18:57:18 +0800

    调整组件位置

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,6 +1,6 @@
-import { Input } from './basic/input';
-import { Mask } from './basic/mask';
-import { Message } from './basic/message';
+import { Input } from './input';
+import { Mask } from './mask';
+import { Message } from './message';
 import { Modal } from './modal';
 
 export { Input, Mask, Message, Modal };
--- src/widgets/modal/index.tsx
+++ src/widgets/modal/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // components
-import { Mask } from '../basic/mask/';
+import { Mask } from '../mask';
 // css
 import './index.less';
 
