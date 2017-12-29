440a68cbd91025f6198fe71c7669fac7e91840f8
wangfanghua <wangfanghua@jk.cn>
2017-12-29 17:38:05 +0800

    组件： 1、添加menu组件目录

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -5,5 +5,6 @@ import { Modal } from './modal/Modal';
 import { Header } from './header/Header';
 import { Slidebar } from './slidebar/Slidebar';
 import { View } from './view/View';
+import { Menu } from './menu/Menu';
 
-export { Header, Slidebar, View, Input, Mask, Message, Modal };
+export { Header, Slidebar, View, Menu, Input, Mask, Message, Modal };
---
+++ src/widgets/menu/Menu.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class Menu extends React.Component<{}, never> {
+    render() {
+        return <div>
+            menu
+        </div>
+    }
+}
