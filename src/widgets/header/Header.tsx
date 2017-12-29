ddeb8b0baec98d84b4044b9b3883b7eaad6d122d
wangfanghua <wangfanghua@jk.cn>
2017-12-29 18:32:52 +0800

    组件: 1、header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
+import './Header.less';
+
+
+interface IHeaderProps {
+    logo?: React.ReactNode;
+    title?: string;
+    buttonList?: React.ReactNode;
+}
 
 export class Header extends React.Component<{}, never> {
     render() {
-        return <div>
-            header
+        return <div className={"header"}>
+            <span className={"logo"}>
+                logo
+            </span>
+            <span className={"title"}>
+                title
+            </span>
+            <span className={"buttons"}>
+                buttons
+            </span>
         </div>
     }
 }
