5642e24d36cdb104843b7b5f8bc3ef1636571c70
wangfanghua <wangfanghua@jk.cn>
2017-12-29 14:47:57 +0800

    前端数据模型： 1、去掉不用的代码，重新构思整个流程

--- src/pages/first/actions/firstActions.ts
+++ src/pages/first/actions/firstActions.ts
@@ -8,31 +8,15 @@ export const FETCH_ACCOUNT = 'FETCH_ACCOUNT';
 export let fetchAccountList = () => {
 	let action: any = {
 		type: BEGIN_AJAX,
-		data: {}
+		payload: {}
 	};
 	return action;
-
-	// return (dispatch, getState) => {
-	// 	let url = '/v1/accounts';
-	// 	let options = {};
-	// 	dispatch(action);
-	// 	return get(url, options).then((res: any) => {
-	// 		action.type = FETCH_ACCOUNT_LIST;
-	// 		action.data = res.data;
-	// 		return dispatch(action);
-	// 	},
-	// 		(err) => {
-	// 			action.type = ERROR_AJAX;
-	// 			action.data = err;
-	// 			return dispatch(action);
-	// 		});
-	// };
 };
 
 export let fetchAccount = () => {
 	let action = {
 		type: FETCH_ACCOUNT,
-		data: {}
+		payload: {}
 	};
 	return action;
 }
