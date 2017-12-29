3a843b9822c4908b59e780cf8840b266c2b35ecd
wangfanghua <wangfanghua@jk.cn>
2017-12-29 18:39:27 +0800

    组件： 1、slidebar组件

--- src/widgets/slidebar/Slidebar.tsx
+++ src/widgets/slidebar/Slidebar.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import './Slidebar.less';
+
+interface ISlidebarProps {
+}
 
 export class Slidebar extends React.Component<{}, never> {
     render() {
-        return <div>
+        return <div className={"slidebar"}>
             header
         </div>
     }
