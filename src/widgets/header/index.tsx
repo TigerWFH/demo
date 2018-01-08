f9a7d274ebffcf405cab9e9ab1c3e97795809732
wangfanghua <wangfanghua@jk.cn>
2018-01-08 11:15:25 +0800

    组件： 1、完善按钮细节

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -6,21 +6,21 @@ interface IHeaderProps {
     logo?: React.ReactNode;
     title?: string;
     buttonList?: React.ReactNode;
-    onHideSlidebar?: ()=>void;
+    onHideSlidebar?: () => void;
 }
 
 export class Header extends React.Component<IHeaderProps, never> {
     onClick = () => {
-        let { onHideSlidebar} = this.props;
+        let { onHideSlidebar } = this.props;
         onHideSlidebar && onHideSlidebar();
-    } 
+    }
     render() {
         return <div className={"header"}>
             <span className={"logo"}>
                 logo
-                <button onClick={this.onClick}>
-                    12313123
-                </button>
+            </span>
+            <span className={"hide-slidebar"}
+                onClick={this.onClick}>
             </span>
             <span className={"title"}>
                 title
