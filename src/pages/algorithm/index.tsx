7db285509d5df833151fd2625ec0605e91d93c31
wangfanghua <wangfanghua@jk.cn>
2018-01-16 17:59:11 +0800

    修改路径错误

--- src/pages/algorithm/index.tsx
+++ src/pages/algorithm/index.tsx
@@ -1,2 +1,23 @@
-import AlgorithmUI from './container';
-export default AlgorithmUI;
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+import { Input } from '../../widgets';
+
+import './index.less';
+interface IProps { }
+interface IState { }
+class AlgorithmUI extends React.Component<IProps, IState>{
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="mm">
+                algorithm
+            </div>
+        )
+    }
+}
+
+export default connect()(AlgorithmUI);
