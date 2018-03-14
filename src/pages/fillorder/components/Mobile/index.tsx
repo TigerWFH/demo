22740c49a7e244e7a5fac272f47a4b7839e18b7e
wangfanghua <wangfanghua@jk.cn>
2018-03-14 18:25:11 +0800

    Mobile组件完成

--- src/pages/fillorder/components/Mobile/index.tsx
+++ src/pages/fillorder/components/Mobile/index.tsx
@@ -4,6 +4,7 @@ import './index.less';
 
 interface IMobileProps {
     tip?: string;
+    onChange?: (value) => {};
 }
 
 interface IMobileState {
@@ -14,9 +15,28 @@ class Mobile extends React.Component<IMobileProps, IMobileState>{
     constructor(props) {
         super(props);
         this.state = {
-            isShowTip: true
+            isShowTip: false
         };
     }
+
+    validateValue(value) {
+        let rule = /^1[0-9]{10}/;
+        return rule.test(value);
+    }
+
+    onChange = (target) => {
+        let event = target.nativeEvent;
+        let value = event.target.value;
+        let { onChange } = this.props;
+        if (typeof onChange === 'function') {
+            onChange(value);
+        }
+        let result = this.validateValue(value);
+        this.setState({
+            isShowTip: !result
+        });
+    }
+
     render() {
         let { isShowTip } = this.state;
         let { tip } = this.props;
@@ -28,7 +48,11 @@ class Mobile extends React.Component<IMobileProps, IMobileState>{
                     <span className={'title'}>
                         填写手机号：
                     </span>
-                    <input className={'mobile'} type="number" />
+                    <input className={'mobile'}
+                        type="phone-pad"
+                        placeholder={'请输入手机号'}
+                        maxLength={11}
+                        onChange={this.onChange} />
                     {
                         isShowTip ?
                             <span className={'error'}>
