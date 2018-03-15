54c0f137d362b4af3f3fdec88fb2dc1bd9d34a0d
wangfanghua <wangfanghua@jk.cn>
2018-03-15 16:34:34 +0800

    title和usage过长的处理

--- src/pages/fillorder/components/GoodsList/index.tsx
+++ src/pages/fillorder/components/GoodsList/index.tsx
@@ -1,14 +1,54 @@
 import * as React from 'react';
+import './index.less';
 
 interface IGoodsListProps {
     goodsList: Array<any>;
 }
 
 class GoodsList extends React.Component<IGoodsListProps, never> {
+    renderGoodsItem = (item, index) => {
+        let elem = <div className={'goodsContainer'}
+            key={'goods:' + index}>
+            <span className={'sortContainer'}>
+                <span className={'sort'}>
+                    {index}
+                </span>
+                <span className={'sortSymbol'}>
+                    .
+                </span>
+            </span>
+            <img className={'img'}
+                src={''} />
+            <span className={'contentContainer'}>
+                <span className={'title'}>
+                    名字
+                </span>
+                <span className={'usage'}>
+                    用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量用法用量
+                </span>
+                <span className={'priceContainer'}>
+                    <span className={'price'}>
+                        $12.02
+                    </span>
+                    <span className={'amount'}>
+                        x12
+                    </span>
+                </span>
+            </span>
+        </div>
+
+        return elem;
+    }
     render() {
+        let { goodsList } = this.props;
         return (
             <div className={'rootContainerGoodsList'}>
-                GoodsList
+                {
+                    Array.isArray(goodsList) ?
+                        goodsList.map((item, index) => {
+                            return this.renderGoodsItem(item, index)
+                        }) : null
+                }
             </div>
         )
     }
--- src/pages/fillorder/index.tsx
+++ src/pages/fillorder/index.tsx
@@ -30,13 +30,22 @@ class FillOrder extends React.Component<IFillOrderProps, IFillOrderState>{
     render() {
         let tip = '温馨提示：订单支付成功后通过手机发送取药码，请凭借取药码于2小时内完成取药，过时将取消订单,订单支付成功后通过手机发送取药码，请凭借取药码于2小时内完成取药，过时将取消订单';
         let phone = '123131231220000';
+        let item = {
+            title: 'monkey的小屋',
+            img: require('../../common/res/images/fhw3937.jpg'),
+            price: '123',
+            usage: '用法用量用法用量用法用量用法用量用法用量',
+            amount: '12',
+            skuId: '',
+            spuId: ''
+        };
         return <div className={'rootContainerFillOrder'}>
             <QualityAssurance />
             <Mobile disabled={false}
                 mobile={phone}
                 tip={tip}
                 onChange={this.onPhone} />
-            <GoodsList goodsList={[]}/>
+            <GoodsList goodsList={[item, item]}/>
             <Footer amount={3}
                 totalFee={200} />
         </div>
