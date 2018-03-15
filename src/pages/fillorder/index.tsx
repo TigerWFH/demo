b2f60428a5d52ae66a8a8ccc86c97e6522cf21fb
wangfanghua <wangfanghua@jk.cn>
2018-03-15 18:00:40 +0800

    修正总数量和总价

--- src/pages/fillorder/index.tsx
+++ src/pages/fillorder/index.tsx
@@ -36,8 +36,12 @@ class FillOrder extends React.Component<IFillOrderProps, IFillOrderState>{
     }
     render() {
         let { goodsInfo: { goodsList = [] } } = this.props;
+        let amount = 0;
+        let totalFee = 0;
         goodsList.forEach(elem => {
             elem.img = require('../../common/res/images/fhw3937.jpg');
+            amount += elem.amount;
+            totalFee += elem.amount * elem.price;//本应该后台计算
         });
         let tip = '温馨提示：订单支付成功后通过手机发送取药码，请凭借取药码于2小时内完成取药，过时将取消订单,订单支付成功后通过手机发送取药码，请凭借取药码于2小时内完成取药，过时将取消订单';
 
@@ -46,8 +50,8 @@ class FillOrder extends React.Component<IFillOrderProps, IFillOrderState>{
             <Mobile tip={tip}
                 onChange={this.onPhone} />
             <GoodsList goodsList={goodsList} />
-            <Footer amount={3}
-                totalFee={200} />
+            <Footer amount={amount}
+                totalFee={totalFee} />
         </div>
     }
 }
