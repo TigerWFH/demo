217b2df18884fdede4407aea9a94252198145c25
wangfanghua <wangfanghua@jk.cn>
2018-03-15 13:59:52 +0800

    footer组件完成

--- src/pages/fillorder/components/Footer/index.tsx
+++ src/pages/fillorder/components/Footer/index.tsx
@@ -2,14 +2,42 @@ import * as React from 'react';
 import './index.less';
 
 interface IFooterProps {
-
+    amount: number;
+    totalFee: number;
+    isShowPyment?: boolean;
 }
 
 class Footer extends React.Component<IFooterProps, never>{
+    onToPayment = () => {
+        // 跳转去支付页面
+    }
     render() {
+        let { isShowPyment = true } = this.props;
+        let amount = this.props.amount || 3;
+        let totalFee = this.props.totalFee || 30.00;
         return (
             <div className={'rootContainerFooter'}>
-                "footer"
+                <span className={'amount'}>
+                    {`共${amount}件，合计`}
+                </span>
+                <span className={'feeContainer'}>
+                    <span className={'symbol'}>
+                        ￥
+                    </span>
+                    <span className={'totalFee'}>
+                        {
+                            totalFee.toFixed(2)
+                        }
+                    </span>
+                </span>
+                {
+                    isShowPyment ?
+                        <button className={'payment'}
+                            onClick={this.onToPayment}>
+                            去支付
+                        </button> :
+                        null
+                }
             </div>
         )
     }
