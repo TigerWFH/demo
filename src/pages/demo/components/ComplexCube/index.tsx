99ad0cd1bf83b78cac4a404673d75642d4f19437
wangfanghua <wangfanghua@jk.cn>
2018-03-29 16:58:21 +0800

    ComplexCube组件初始化，目录结构

--- src/pages/demo/components/ComplexCube/index.tsx
+++ src/pages/demo/components/ComplexCube/index.tsx
@@ -6,7 +6,12 @@ interface IComplexCubeProps { }
 class ComplexCube extends React.Component<IComplexCubeProps, never> {
     render() {
         return <div className='complexCube'>
-        Cube
+            <div className='wrapper'>
+                <div className='container container1'>
+                </div>
+                <div className='container container2'>
+                </div>
+            </div>
         </div>
     }
 }
