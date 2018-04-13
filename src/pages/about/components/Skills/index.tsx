bb645466f7ed3e68e554031c8b7069c2bf831469
wangfanghua <wangfanghua@jk.cn>
2018-04-13 16:59:19 +0800

    一些知识点抽象

--- src/pages/about/components/Skills/index.tsx
+++ src/pages/about/components/Skills/index.tsx
@@ -1,3 +1,17 @@
+
+/*
+抽象出数据:
+1、编程语言：Javascript、Html、Css、C、C++、
+2、常用库：React、Redux、React-router、mobx、chartjs
+3、工程化工具：webpack，node，rollup、gulp
+4、熟悉平台：window、ubuntu、mac
+5、编程IDE：visual studio code
+6、项目经历：toB的项目：broker work，CRM系统；保证金系统一期
+			toC的项目：trader work、trader work mobile；plugin项目，fillorderout项目
+7、自学能力：
+8、其它：运维能力、photoshop、scheth
+*/ 
+
 import * as React from 'react';
 import './index.less';
 import * as Chart from 'chart.js';
@@ -8,16 +22,56 @@ interface ISkillsProps {
 
 class Skills extends React.Component<ISkillsProps, never> {
 	context: any;
-	
+
 	componentDidMount() {
 		if (!this.context) {
 			return null;
 		}
 		let ctx = this.context.getContext('2d');
+		let mockData = {
+			labels: ["javascript", "http", "c/c++", "react&other", "redux", "webpack"],
+			datasets: [{
+				label: 'title of Votes',
+				data: [3, 3, 6, 3, 3, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1,
+				yAxisId: 'monkey1'
+			}]
+		};
 		let options: any = {
 			type: 'bar',
-			data: [],
-			options: null
+			data: mockData,
+			options: {
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								id: 'monkey1',
+								beginAtZero:true
+							}
+						}
+					]
+				},
+				title: {
+					display: true,
+					text: 'MonkeyWong的技能图谱'
+				}
+			}
 		};
 		let lineChart = new Chart(ctx, options);
 	}
@@ -30,11 +84,13 @@ class Skills extends React.Component<ISkillsProps, never> {
 	render() {
 		return (
 			<div className={'skillsRoot'}>
-				<canvas width="960" height="480"
+				<canvas
+					width="960"
+					height="480"
 					ref={(context) => {
 						this.context = context;
 					}}
-				/>
+				></canvas>
 				<span className={'direction'} onClick={this.onToNext}>
 					︾
 				</span>
