3ea9c99520c6c3a4e80eeebb0164420148eadb00
wangfanghua <wangfanghua@jk.cn>
2018-06-19 19:16:33 +0800

    样式调整

--- src/pages/state/components/Product/index.tsx
+++ src/pages/state/components/Product/index.tsx
@@ -18,17 +18,17 @@ function Product(props) {
 				<div className="product-description-title">{product.name + product.spec}</div>
 				<div className="product-description-usage">{product.usage}</div>
 				<div className="product-description-op-container">
-					<span className="product-price-gold-container">
+					<div className="product-price-gold-container">
 						<span className="product-price-container">
 							<span className="product-price-symbol">{PRICE_SYMBOL}</span>
 							<span className="product-price">{product.price}</span>
 						</span>
 						<span className="product-health-gold">{HEALTH_GOLD_TEXT}</span>
-					</span>
-					<span className="product-instruction-amount-container">
+					</div>
+					<div className="product-instruction-amount-container">
 						<span className="product-instruction">{INSTRUCTION_TEXT}</span>
-						<span className="product-amount">X{product.piece}</span>
-					</span>
+						<span className="product-amount">x{product.piece}</span>
+					</div>
 					<div />
 				</div>
 			</div>
