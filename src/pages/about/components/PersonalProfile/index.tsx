3730696c831ab9e9857feedf5e13017f17d502b1
monkey-home <monkey@ubuntu.home>
2018-08-12 19:41:21 +0800

    add label space

--- src/pages/about/components/PersonalProfile/index.tsx
+++ src/pages/about/components/PersonalProfile/index.tsx
@@ -16,15 +16,15 @@ class PersonalProfile extends React.Component<IPersonalProfileProps, never> {
 	render() {
 		return (
 			<div className={'pp-root'}>
-				<div>
+				<div className={'pp-container'}>
 					<div className={'pp-label'}>大家好,我是MonkeyWong</div>
 					<div className={'pp-sublabel'}>目前居住在中国上海</div>
 				</div>
-				<div >
+				<div className={'pp-container'}>
 					<div className={'pp-label'}>我是一名前端开发工程师</div>
 					<div className={'pp-sublabel'}>目前工作在平安好医生</div>
 				</div>
-				<div>
+				<div className={'pp-container'}>
 					<div className={'pp-label'}>
 						C++使用了4年，做了自己的两个毕业设计
 					</div>
