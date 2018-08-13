3dc63a2ba47e465ecfa87a1386f45846d7f572bd
monkey-home <monkey@ubuntu.home>
2018-08-13 22:12:31 +0800

    footer

--- src/pages/about/components/Footer/index.tsx
+++ src/pages/about/components/Footer/index.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import './index.less';
 
-interface IFooterProps {}
+interface IFooterProps { }
 
 class Footer extends React.Component<IFooterProps, never> {
     render() {
         return (
-            <div className={'ppFooter'}>
-                Footer
+            <div className={'pp-footer'}>
+                <span className={'pp-hello'}>打个招呼</span>
+                <div className={'pp-content'}>
+                    <div className={'pp-hope'}>
+                        <div>想结交<span>新朋友</span></div>
+                        <div>想做<span>有趣</span>的事情</div>
+                    </div>
+                    <div className={'pp-contact'}>
+                        <span>如何找到我...</span>
+                        <div>+8617612151221</div>
+                        <div>fanghuawfh@gmail.com</div>
+                    </div>
+                </div>
             </div>
         )
     }
