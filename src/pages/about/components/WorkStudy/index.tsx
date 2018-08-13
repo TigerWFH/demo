84d10bca028408ac02e33e3ba5c60d0e829e505c
wangfanghua <wangfanghua@jk.cn>
2018-08-13 10:30:26 +0800

    工作经历初步布局

--- src/pages/about/components/WorkStudy/index.tsx
+++ src/pages/about/components/WorkStudy/index.tsx
@@ -9,74 +9,28 @@ interface IWorkExperienceProps {
 
 class WorkExperience extends React.Component<IWorkExperienceProps, never> {
 	context: any;
+	pieChart: any;
 	componentDidMount() {
 		if (!this.context) {
 			return null;
 		}
 		let ctx = this.context.getContext('2d');
 		let mockData = {
-			xLabels: [ 'javascript', 'http', 'c/c++', 'react&other', 'redux', 'webpack', 'rollup' ],
-			yLabels: [ '精通', '掌握', '熟悉', '了解', '未知' ],
 			datasets: [
 				{
-					label: '技能值',
-					data: [ '掌握', '熟悉', '熟悉', '熟悉', '熟悉', '熟悉', '了解' ],
-					backgroundColor: [
-						'rgba(255, 99, 132, 0.2)',
-						'rgba(54, 162, 235, 0.2)',
-						'rgba(255, 206, 86, 0.2)',
-						'rgba(75, 192, 192, 0.2)',
-						'rgba(153, 102, 255, 0.2)',
-						'rgba(255, 159, 64, 0.2)'
-					],
-					borderColor: [
-						'rgba(255,99,132,1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)'
-					],
-					borderWidth: 1
+					data: [ 4, 2, 1, 1, 8 ],
+					backgroundColor: {},
+					label: '数据'
 				}
-			]
+			],
+			labels: [ '安阳工学院', '大连民族大学', '云之轩', '领壹金融', '平安好医生' ]
 		};
 		let options: any = {
-			type: 'line',
+			type: 'pie',
 			data: mockData,
-			options: {
-				scales: {
-					xAxes: [
-						{
-							scaleLabel: {
-								display: true,
-								labelString: '技能种类'
-							}
-						}
-					],
-					yAxes: [
-						{
-							type: 'category',
-							scaleLabel: {
-								display: true,
-								labelString: '熟练程度'
-							},
-							ticks: {
-								beginAtZero: true
-							}
-						}
-					]
-				},
-				title: {
-					display: true,
-					text: 'MonkeyWong的技能图谱'
-				},
-				animation: {
-					duration: 5000
-				}
-			}
+			options: {}
 		};
-		let lineChart = new Chart(ctx, options);
+		this.pieChart = new Chart(ctx, options);
 	}
 	onToNext = () => {
 		let { onToNext } = this.props;
