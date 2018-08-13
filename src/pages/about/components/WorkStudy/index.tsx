8de0d6c85267956cae0dc9bd6c55209c5e1b2e96
wangfanghua <wangfanghua@jk.cn>
2018-08-13 13:58:38 +0800

    chart的一些交互操作

--- src/pages/about/components/WorkStudy/index.tsx
+++ src/pages/about/components/WorkStudy/index.tsx
@@ -7,9 +7,29 @@ interface IWorkExperienceProps {
 	onToNext?: Function;
 }
 
-class WorkExperience extends React.Component<IWorkExperienceProps, never> {
+interface IWorkExperienceState {
+	index?: React.ReactText;
+}
+
+const LABELS = [ '平安好医生', '领壹金融', '云之轩', '大连民族大学', '安阳工学院' ];
+
+const MAP_NAME_TO_COMPONENT = new Map([
+['平安好医生', null],
+['领壹金融', null],
+['云之轩', null],
+['大连民族大学', null],
+['安阳工学院', null],
+]);
+
+class WorkExperience extends React.Component<IWorkExperienceProps, IWorkExperienceState> {
 	context: any;
 	pieChart: any;
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			index: 0
+		};
+	}
 	componentDidMount() {
 		if (!this.context) {
 			return null;
@@ -22,13 +42,65 @@ class WorkExperience extends React.Component<IWorkExperienceProps, never> {
 					backgroundColor: [ '#1E90FF', '#00A3FE', '#FF0000', '#FF6347', '#FFA500' ]
 				}
 			],
-			labels: [ '平安好医生', '领壹金融', '云之轩', '大连民族大学', '安阳工学院' ]
+			labels: LABELS
 		};
 		let options: any = {
 			type: 'pie',
 			data: mockData,
 			options: {
-				responsive: true
+				// 统一的配置项
+				// 响应式布局相关配置
+				// responsive: true,
+				// responsiveAnimationDuration: 20,
+				// maintainAspectRatio: true,
+				// onResize: ()=>{},
+				// 图表标题
+				title: {
+					display: true,
+					text: '主要的学习工作经历'
+				},
+				// 图表数据集概略图
+				legend: {
+					position: 'left',
+					fullWidth: true,
+					onClick: (e, item) => {
+						const { text } = item;
+						this.setIndex(text);
+					},
+					onHover: (e, item) => {
+						if (e.target.style.cursor !== 'pointer') {
+							e.target.style.cursor = 'pointer';
+						}
+					}
+				},
+				// hover: {
+				// 	onHover: (e)=>{
+				// 		console.log("=======", e);
+				// 	}
+				// },
+				//
+				tooltips: {
+					// mode: 'index',
+					callbacks: {
+						title: (items, data) => {
+							const { index = 0 } = items[0];
+							this.setIndex(data.labels[index]);
+							return data.labels[index];
+						},
+						label: (items, data) => {
+							const { index = 0 } = items;
+							const label = data.datasets[0].data[index] === 8 ? '至今' : data.datasets[0].data[index];
+							return label;
+						}
+					}
+				},
+				// 动画
+				animation: {},
+				// 元素
+				elements: {},
+				// 布局
+				layout: {}
+				// 具体图表的配置项
 			}
 		};
 		this.pieChart = new Chart(ctx, options);
@@ -38,6 +110,12 @@ class WorkExperience extends React.Component<IWorkExperienceProps, never> {
 		if (typeof onToNext === 'function') {
 			onToNext();
 		}
+	};
+
+	setIndex = (index) => {
+		this.setState({
+			index
+		});
 	};
 	render() {
 		return (
@@ -50,7 +128,7 @@ class WorkExperience extends React.Component<IWorkExperienceProps, never> {
 							}}
 						/>
 					</div>
-					<div className={'we-content'}>内容展示</div>
+					<div className={'we-content'}>内容展示:{this.state.index}</div>
 				</div>
 				<span className={'direction'} onClick={this.onToNext}>
 					︾
