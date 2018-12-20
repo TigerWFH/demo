1a93f51fb86904ce482cc810a689a9d4b029bc7d
wangfanghua618 <wangfanghua618@jk.cn>
2018-12-20 15:39:10 +0800

    一些有趣的问题

---
+++ src/widgets/loading/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export class Loading extends React.Component<{}, never> {
+    render() {
+        return <div>
+            Loading...
+        </div>
+    }
+}
+
+export default Loading;
