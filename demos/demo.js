b67e4717decbb39443dc9ac7a3f6367b9172ea99
wangfanghua618 <wangfanghua618@pingan.com.cn>
2019-07-31 18:09:53 +0800

    补充两个工具函数： 1、getParamFromUrlByName：根据key获取url上的参数，同名参数，前面的优先级高于后面的 2、getParamFromSearchByName：根据key获取location.search上的查询参数

--- demos/demo.js
+++ demos/demo.js
@@ -1,10 +1,11 @@
-let o = {
-    name: "monkey",
-    age: 12
-};
-
-function get(b) {
-    return b;
+function getSearchParamByName(name) {
+    const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    const res = window.location.search.substr(1).match(reg);
+    if (res != null) {
+        return unescape(res[2]);
+    }
+    return null;
 }
 
-o === get(o);
+let url = 'https://www.dev.pajkdc.com/mall-flash/index.html?channel=123&storeId=123&sellerId=123#/health?channel=123&storeId=123&sellerId=123';
+console.log("storeId", getSearchParamByName());
--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -1,3 +1,47 @@
+/**
+ * @description 获取url上的查询参数（包括hash上的查询参数）
+ * @param {string} name 查询关键字
+ * @param {string} url 查询url
+ * 
+ * @returns {string | undefined} 返回结果，查询不到返回undefined(因为JSON序列化会忽略值为undefined的字段，节省流量)
+ */
+export function getParamFromUrlByName(name: string, url?: string) {
+	if (!url || !url.length) {
+		url = window.location.href;
+	}
+
+	name = name.replace(/[\[\]]/g, '\\$&');
+	// 只取第一个符合name的参数值
+	const regexp = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+	const resultList = regexp.exec(url);
+	let result = undefined;
+
+	if (Array.isArray(resultList) && resultList[2]) {
+		result = resultList[2];
+	}
+
+	return result;
+}
+/**
+ * @description 根据url原始的location.search查询对应的参数
+ * @param {string} name 查询关键字
+ * 
+ * @returns {string | undefined} 返回结果，查询不到返回undefined
+ */ 
+export function getParamFromSearchByName(name: string) {
+	const search = window.location.search.slice(1);
+	let result = undefined;
+	if (search) {
+		const regexp = new RegExp(`(^|&)${name}=(([^&]*)|&|$)`);
+		const resultList = search.match(regexp);
+		if (Array.isArray(resultList)) {
+			result = resultList[2];
+		}
+	}
+
+	return result;
+}
+
 export function mergeString(propName: string, origin: string) {
 	let cn = origin;
 	if (propName) {
